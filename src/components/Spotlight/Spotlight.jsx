4c4740fc95088c45735914424994f0b42a566ad5
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-18 16:42:56 +1000

    fix: updated spotlight-component with productcards

--- src/components/Spotlight/Spotlight.jsx
+++ src/components/Spotlight/Spotlight.jsx
@@ -1,25 +1,25 @@
 import styles from './Spotlight.module.css';
+import ProductCard from '../ProductCard/ProductCard';
+import data from '../../assets/products.json';
 
 const Spotlight = () => {
+  const products = data.products;
+
   return (
     <article className={styles.spotlight}>
       <h3>Featured Products</h3>
       <div className={styles.content}>
-        <div className={styles.card}>
-          <img src="./products/product01.jpg" alt="featured product image" />
-          <h5>"Warrior Blossom" - Premium Soft-Washed Long-Sleeve Shirt</h5>
-          <p>from $39.50</p>
-        </div>
-        <div className={styles.card}>
-          <img src="./products/product02.jpg" alt="featured product image" />
-          <h5>"Goated" - T Shirt</h5>
-          <p>from $39.50</p>
-        </div>
-        <div className={styles.card}>
-          <img src="./products/product03.jpg" alt="featured product image" />
-          <h5>"Warrior Blossom" - Pull-Over Hoodie</h5>
-          <p>from $39.50</p>
-        </div>
+        {
+          products.slice(1, 4).map((product) => 
+          <ProductCard 
+            key={product.productID}
+            tag={product.productTag}
+            image={product.productImg}
+            title={product.productName}
+            price={product.productPrice}
+          />
+        )
+        }
       </div>
     </article>
   )
