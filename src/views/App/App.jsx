8fb79ace9627f8dc87ffb10fa1adeb692e8007c1
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-21 12:33:22 +1000

    feat: add ProductList-component

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,14 +1,10 @@
 import AdBanner from '../../components/AdBanner/AdBanner';
 import HeroBanner from '../../components/HeroBanner/HeroBanner';
 import Navbar from '../../components/Navbar/Navbar';
-import ProductCard from '../../components/ProductCard/ProductCard';
 import Spotlight from '../../components/Spotlight/Spotlight';
 import styles from './App.module.css';
-import data from '../../assets/products.json';
-
 
 const App = () => {
-  const products = data.products;
 
   return (
     <main className={styles.app}>
