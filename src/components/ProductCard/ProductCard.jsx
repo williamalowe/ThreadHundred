9a7ee7fbc851459d93fea54b4fef441b33779a2d
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-20 15:42:00 +1000

    styles: prettier

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,15 +1,22 @@
 import styles from "./ProductCard.module.css";
+import { motion } from "framer-motion";
 
 const ProductCard = ({ tag, image, title, price }) => {
   return (
-    <div className={styles.card}>
+    <motion.div
+      className={styles.card}
+      whileHover={{
+        scale: 1.2,
+        zIndex: 100,
+      }}
+    >
       {tag && <div className={styles.tag}>{tag}</div>}
       <img src={image} alt="product image" />
       <div className={styles.description}>
         <h3>{title}</h3>
         <p>from ${price}</p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
