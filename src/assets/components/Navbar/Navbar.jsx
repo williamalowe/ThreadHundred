3b37f0e862d41b46235c02c3f6f3a9d9130f981e
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-21 12:55:07 +1000

    styles: add navbar motion

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,20 +1,70 @@
 import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <NavLink to='/'>
-        <div className='link'>Home</div>
+        <motion.div 
+          className='link'
+          whileHover={{
+            boxShadow: 0,
+            x: 4,
+            y: 4
+          }}
+          whileTap={{
+            scale: 0.9
+          }}
+        >
+          Home
+        </motion.div>
       </NavLink>
       <NavLink to='/shop'>
-        <div className='link'>Shop</div>
+        <motion.div 
+          className='link'
+          whileHover={{
+            scale: 1.1,
+            boxShadow: 0,
+            x: 4,
+            y: 4
+          }}
+          whileTap={{
+            scale: 0.9
+          }}
+        >
+          Shop
+        </motion.div>
       </NavLink>
       <NavLink to='/cart'>
-        <div className='link'>Cart</div>
+        <motion.div 
+          className='link'
+          whileHover={{
+            boxShadow: 0,
+            x: 4,
+            y: 4
+          }}
+          whileTap={{
+            scale: 0.9
+          }}
+        >
+          Cart
+        </motion.div>
       </NavLink>
       <NavLink to='/contact'>
-      <div className='link'>Contact</div>
+      <motion.div 
+      className='link'
+      whileHover={{
+        boxShadow: 0,
+        x: 4,
+        y: 4
+      }}
+      whileTap={{
+        scale: 0.9
+      }}
+      >
+        Contact
+      </motion.div>
       </NavLink>
     </nav>
   )
