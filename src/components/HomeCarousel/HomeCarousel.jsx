12d60e937095810164ed1d79dcb0b5715a38cf93
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-21 14:41:32 +1000

    styles: update carousel motion

--- src/components/HomeCarousel/HomeCarousel.jsx
+++ src/components/HomeCarousel/HomeCarousel.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import styles from './HomeCarousel.module.css';
-import { motion, AnimatePresence } from 'framer-motion';
+import { useState, useEffect } from "react";
+import styles from "./HomeCarousel.module.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 const HomeCarousel = () => {
   const [position, setPosition] = useState(0);
@@ -11,75 +11,90 @@ const HomeCarousel = () => {
     } else {
       setPosition(position + 1);
     }
-  }
+  };
 
   useEffect(() => {
     setTimeout(() => {
       next();
-    }, "5000")
-  }, [position])
-  
+    }, "5000");
+  }, [position]);
+
   return (
     <div className={styles.carousel}>
-      <AnimatePresence mode='popLayout'>
-      {
-        position === 0 &&
-        <motion.div 
-          className={styles.wrapper}
-          exit={{
-            x: '-33vw'
-          }}
+      <AnimatePresence mode="popLayout">
+        {position === 0 && (
+          <motion.div
+            className={styles.wrapper}
+            exit={{
+              x: "-33vw",
+            }}
+            transition={{
+              duration: 1,
+              type: "spring",
+            }}
           >
-          <div className={styles.first}></div>
-          <div className={styles.second}></div>
-          <div className={styles.third}></div>
-        </motion.div>
-      }
+            <div className={styles.first}></div>
+            <div className={styles.second}></div>
+            <div className={styles.third}></div>
+          </motion.div>
+        )}
       </AnimatePresence>
-      <AnimatePresence mode='popLayout'>
-      {
-        position === 1 &&
-        <motion.div className={styles.wrapper}
-        
-          exit={{
-            x: '-33vw'
-          }}>
-          <div className={styles.second}></div>
-          <div className={styles.third}></div>
-          <div className={styles.fourth}></div>
-        </motion.div>
-      }
+      <AnimatePresence mode="popLayout">
+        {position === 1 && (
+          <motion.div
+            className={styles.wrapper}
+            exit={{
+              x: "-33vw",
+            }}
+            transition={{
+              duration: 1,
+              type: "spring",
+            }}
+          >
+            <div className={styles.second}></div>
+            <div className={styles.third}></div>
+            <div className={styles.fourth}></div>
+          </motion.div>
+        )}
       </AnimatePresence>
-      <AnimatePresence mode='popLayout'>
-      {
-        position === 2 &&
-        <motion.div className={styles.wrapper}
-        
-          exit={{
-            x: '-33vw'
-          }}>
-          <div className={styles.third}></div>
-          <div className={styles.fourth}></div>
-          <div className={styles.first}></div>
-        </motion.div>
-      }
+      <AnimatePresence mode="popLayout">
+        {position === 2 && (
+          <motion.div
+            className={styles.wrapper}
+            exit={{
+              x: "-33vw",
+            }}
+            transition={{
+              duration: 1,
+              type: "spring",
+            }}
+          >
+            <div className={styles.third}></div>
+            <div className={styles.fourth}></div>
+            <div className={styles.first}></div>
+          </motion.div>
+        )}
       </AnimatePresence>
-      <AnimatePresence mode='popLayout'>
-      {
-        position === 3 &&
-        <motion.div className={styles.wrapper}
-        
-          exit={{
-            x: '-33vw'
-          }}>
-          <div className={styles.fourth}></div>
-          <div className={styles.first}></div>
-          <div className={styles.second}></div>
-        </motion.div>
-      }
+      <AnimatePresence mode="popLayout">
+        {position === 3 && (
+          <motion.div
+            className={styles.wrapper}
+            exit={{
+              x: "-33vw",
+            }}
+            transition={{
+              duration: 1,
+              type: "spring",
+            }}
+          >
+            <div className={styles.fourth}></div>
+            <div className={styles.first}></div>
+            <div className={styles.second}></div>
+          </motion.div>
+        )}
       </AnimatePresence>
     </div>
-  )
-}
+  );
+};
 
-export default HomeCarousel
+export default HomeCarousel;
