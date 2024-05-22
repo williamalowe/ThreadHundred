c295e5de6dce3ca998d921709db890de570030e8
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-22 13:37:35 +1000

    feat: add shop banner

--- src/views/Shop/Shop.jsx
+++ src/views/Shop/Shop.jsx
@@ -3,7 +3,14 @@ import styles from './Shop.module.css';
 const Shop = () => {
   return (
     <div className={styles.shop}>
-      
+      <div className={styles.content}>
+        <div className={styles.banner}>
+          All Products
+        </div>
+        <div className={styles.products}>
+          
+        </div>
+      </div>
     </div>
   )
 }
