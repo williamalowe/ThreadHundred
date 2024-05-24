595256e8b1dc345e3c481fb980ec0522d28230be
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-24 16:42:27 +1000

    styles: add sidecart mobile styles

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -28,6 +28,7 @@ const SideCart = () => {
   }, [cart]);
 
   return (
+    <>
     <div className={styles.sidecart}>
       <AnimatePresence mode="popLayout">
         {!showCart && (
@@ -129,6 +130,109 @@ const SideCart = () => {
         )}
       </AnimatePresence>
     </div>
+    <div className={styles.mobile}>
+      <AnimatePresence mode="popLayout">
+        {!showCart && (
+          <motion.button
+            className={styles.mob_button}
+            onClick={() => setShowCart(true)}
+            initial={{
+              y: 100,
+            }}
+            animate={{
+              y: 0,
+            }}
+            exit={{
+              y: 100,
+            }}
+            whileHover={{
+              scale: 1.1,
+            }}
+            whileTap={{
+              scale: 0.9,
+            }}
+          >
+            {cart.length}
+            <FontAwesomeIcon icon={faCartShopping} />
+          </motion.button>
+        )}
+      </AnimatePresence>
+      <AnimatePresence mode="popLayout">
+        {
+          showCart && 
+          <motion.div 
+            className={styles.mob_bar}
+            initial={{
+              height: 0
+            }}
+            animate={{
+              height: 200
+            }}
+            exit={{
+              y: 200
+            }}
+          >
+            <div className={styles.header}>
+              <h3>Your Cart</h3>
+              <p>{cart.length} Items</p>
+            </div>
+            <div className={styles.products}>
+              {cart.map((item, index) => (
+                <CartItem
+                  key={index}
+                  img={item.img}
+                  product={item.product}
+                  price={item.price}
+                />
+              ))}
+            </div>
+            <div className={styles.footer}>
+              <h3>TOTAL: ${total}</h3>
+              <Link to={"/cart"}>
+                <motion.button
+                  whileHover={{
+                    boxShadow: 0,
+                    x: 4,
+                    y: 4,
+                  }}
+                  whileTap={{
+                    scale: 0.9,
+                  }}
+                >
+                  Checkout
+                </motion.button>
+              </Link>
+              </div>
+          </motion.div>
+        }
+      </AnimatePresence>
+      <AnimatePresence mode="popLayout">
+        {showCart && (
+          <motion.button
+            className={styles.mob_close_button}
+            onClick={() => setShowCart(false)}
+            initial={{
+              y: 100,
+            }}
+            animate={{
+              y: 0,
+            }}
+            exit={{
+              y: 100,
+            }}
+            whileHover={{
+              scale: 1.1,
+            }}
+            whileTap={{
+              scale: 0.9,
+            }}
+          >
+            <FontAwesomeIcon icon={faX} />
+          </motion.button>
+        )}
+      </AnimatePresence>
+    </div>
+    </>
   );
 };
 
