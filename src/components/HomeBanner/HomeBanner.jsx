6e59a3bc6d5530868be3258cde14103e25bd01cd
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-24 14:13:52 +1000

    styles: add homebanner mobile styles

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -3,9 +3,9 @@ import styles from './HomeBanner.module.css';
 const HomeBanner = () => {
   return (
     <div className={styles.banner}>
-      <div><span>Sustainably Printed</span> in Australia since 2011</div>
-      <div><span>Easy 100 day returns</span> on all orders</div>
-      <div><span>Ethically produced</span> and shipped from Melbourne</div>
+      <div>Sustainably Printed <span>in Australia since 2011</span></div>
+      <div>Easy 100 day returns <span>on all orders</span></div>
+      <div>Ethically produced <span>and shipped from Melbourne</span></div>
     </div>
   )
 }
