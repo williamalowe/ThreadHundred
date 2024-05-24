139e732be86a9bd54ed29ef48967c324d642fc62
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-05-24 12:14:42 +1000

    feat: add contactform button

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,14 +1,28 @@
-import styles from './ContactForm.module.css';
+import styles from "./ContactForm.module.css";
+import { motion } from "framer-motion";
 
 const ContactForm = () => {
   return (
     <form className={styles.contact_form} onSubmit={(e) => e.preventDefault()}>
-      <input type="text" placeholder='Name'/>
-      <input type="text" placeholder='Email'/>
-      <input type="text" placeholder='Phone Number'/>
-      <textarea placeholder='Message'></textarea>
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Email" />
+      <input type="text" placeholder="Phone Number" />
+      <textarea placeholder="Message"></textarea>
+      <motion.button
+        whileHover={{
+          scale: 1.1,
+          boxShadow: 0,
+          x: 4,
+          y: 4,
+        }}
+        whileTap={{
+          scale: 0.9,
+        }}
+      >
+        Send
+      </motion.button>
     </form>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
